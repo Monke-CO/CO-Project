09106093ca218e4572231fb22f3dc2415d31b8ad
marc <fmarcandrei@gmail.com>
2023-05-10 20:15:09 +0300

    added namespaces and modified runAbsolute

--- include/benchmark/cpu/gaussLegendreCPU.h
+++ include/benchmark/cpu/gaussLegendreCPU.h
@@ -5,21 +5,33 @@
 #ifndef CO_PROJECT_GAUSSLEGENDRECPU_H
 #define CO_PROJECT_GAUSSLEGENDRECPU_H
 
+namespace BenchMark {
+    namespace Cpu {
+        class gaussLegendreCPU {
+        public:
+            int noDecimals;
+            int result;
+            int noThreads;
+            bool cancel = false;
 
-class gaussLegendreCPU {
-public:
-        int noDecimals;
-        int result;
-        bool cancel = false;
-        void initialize(int numberOfDecimals);
-        void warmup();
-        void runsinglethread();
-        void run(int noDigits);
-        void runAbsolute(bool multithreading ,int noThreads);
-        void runMultiThreaded(int noThreads);
-        void cancelBenchmark();
-        void getResult();
-};
+            void initialize(int numberOfDecimals, int noThreads);
 
+            void warmup();
 
+            void runSingleThread();
+
+            void run(int noDigits);
+
+            void runAbsolute(bool multithreading);
+
+            void runMultiThreaded(int noThreads);
+
+            void cancelBenchmark();
+
+            void getResult();
+        };
+        //namespace cpu
+    }
+    //namespace benchmark
+}
 #endif //CO_PROJECT_GAUSSLEGENDRECPU_H
--- src/benchmark/cpu/gaussLegendreCPU.cpp
+++ src/benchmark/cpu/gaussLegendreCPU.cpp
@@ -7,100 +7,27 @@
 #include <vector>
 #include <thread>
 #include "benchmark/cpu/gaussLegendreCPU.h"
-
-void gaussLegendreCPU::initialize(int numberOfDecimals) {
-    gaussLegendreCPU::noDecimals = numberOfDecimals;
-}
-
-void gaussLegendreCPU::warmup() {
-    gaussLegendreCPU::run(100);
-}
-
-void gaussLegendreCPU::run(int noDigits) {
-    long double a = 1.0, b = 1.0 / sqrt(2.0), t = 1.0 / 4.0, p = 1.0;
-    long double piPrev = 0.0, pi = 0.0;
-
-    int iterations = ceil(log2(noDigits/14.6)); // Compute number of iterations needed
-
-    for (int i = 0; i < iterations; ++i) {
-        if(gaussLegendreCPU::cancel){
-            break; //if the cancel command is given we stop iterating
+namespace BenchMark {
+    namespace Cpu {
+        void gaussLegendreCPU::initialize(int numberOfDecimals, int noThreads) {
+            gaussLegendreCPU::noDecimals = numberOfDecimals;
+            gaussLegendreCPU::noThreads = noThreads;
         }
-        long double aNext = (a + b) / 2.0;
-        long double bNext = sqrt(a * b);
-        long double tNext = t - p * pow(a - aNext, 2.0);
-        long double pNext = 2.0 * p;
 
-        a = aNext;
-        b = bNext;
-        t = tNext;
-        p = pNext;
-
-        piPrev = pi;
-        pi = pow(a + b, 2.0) / (4.0 * t);
-        if (pi == piPrev) {
-            break; // Stop iterating if no change in pi
+        void gaussLegendreCPU::warmup() {
+            gaussLegendreCPU::run(100);
         }
-    }
-}
 
-void gaussLegendreCPU::runsinglethread() {
-    long double a = 1.0, b = 1.0 / sqrt(2.0), t = 1.0 / 4.0, p = 1.0;
-    long double piPrev = 0.0, pi = 0.0;
-
-    int iterations = ceil(log2(gaussLegendreCPU::noDecimals / 14.6)); // Compute number of iterations needed
-
-    for (int i = 0; i < iterations; ++i) {
-        if(gaussLegendreCPU::cancel){
-            break; //if the cancel command is given we stop iterating
-        }
-        long double aNext = (a + b) / 2.0;
-        long double bNext = sqrt(a * b);
-        long double tNext = t - p * pow(a - aNext, 2.0);
-        long double pNext = 2.0 * p;
-
-        a = aNext;
-        b = bNext;
-        t = tNext;
-        p = pNext;
-
-        piPrev = pi;
-        pi = pow(a + b, 2.0) / (4.0 * t);
-        if (pi == piPrev) {
-            break; // Stop iterating if no change in pi
-        }
-    }
-}
-
-void gaussLegendreCPU::cancelBenchmark() {
-    gaussLegendreCPU::cancel = true;
-}
-
-void gaussLegendreCPU::getResult() {
-    //for now empty to be implemented when a gui is added
-}
-
-void gaussLegendreCPU::runMultiThreaded(int noThreads) {
-    std::vector<std::thread> threads(noThreads);
-
-    for (int t = 0; t < noThreads; ++t) {
-        threads[t] = std::thread([&](){
+        void gaussLegendreCPU::run(int noDigits) {
             long double a = 1.0, b = 1.0 / sqrt(2.0), t = 1.0 / 4.0, p = 1.0;
             long double piPrev = 0.0, pi = 0.0;
 
-            int iterations_per_thread = ceil(log2(gaussLegendreCPU::noDecimals / 14.6) / noThreads); // Compute number of iterations per thread
+            int iterations = ceil(log2(noDigits / 14.6)); // Compute number of iterations needed
 
-            int start_iteration = t * iterations_per_thread;
-            int end_iteration = (t + 1) * iterations_per_thread;
-            if (t == noThreads - 1) {
-                end_iteration = ceil(log2(gaussLegendreCPU::noDecimals / 14.6));
-            }
-
-            for (int i = start_iteration; i < end_iteration; ++i) {
+            for (int i = 0; i < iterations; ++i) {
                 if (gaussLegendreCPU::cancel) {
                     break; //if the cancel command is given we stop iterating
                 }
-
                 long double aNext = (a + b) / 2.0;
                 long double bNext = sqrt(a * b);
                 long double tNext = t - p * pow(a - aNext, 2.0);
@@ -117,14 +44,94 @@ void gaussLegendreCPU::runMultiThreaded(int noThreads) {
                     break; // Stop iterating if no change in pi
                 }
             }
-        });
-}
+        }
 
-void gaussLegendreCPU::runAbsolute(bool multithreading, int noThreads) {
-    if (multithreading){
-        gaussLegendreCPU::runMultiThreaded(noThreads);
+        void gaussLegendreCPU::runSingleThread() {
+            long double a = 1.0, b = 1.0 / sqrt(2.0), t = 1.0 / 4.0, p = 1.0;
+            long double piPrev = 0.0, pi = 0.0;
+
+            int iterations = ceil(log2(gaussLegendreCPU::noDecimals / 14.6)); // Compute number of iterations needed
+
+            for (int i = 0; i < iterations; ++i) {
+                if (gaussLegendreCPU::cancel) {
+                    break; //if the cancel command is given we stop iterating
+                }
+                long double aNext = (a + b) / 2.0;
+                long double bNext = sqrt(a * b);
+                long double tNext = t - p * pow(a - aNext, 2.0);
+                long double pNext = 2.0 * p;
+
+                a = aNext;
+                b = bNext;
+                t = tNext;
+                p = pNext;
+
+                piPrev = pi;
+                pi = pow(a + b, 2.0) / (4.0 * t);
+                if (pi == piPrev) {
+                    break; // Stop iterating if no change in pi
+                }
+            }
+        }
+
+        void gaussLegendreCPU::cancelBenchmark() {
+            gaussLegendreCPU::cancel = true;
+        }
+
+        void gaussLegendreCPU::getResult() {
+            //for now empty to be implemented when a gui is added
+        }
+
+        void gaussLegendreCPU::runMultiThreaded(int noThreads) {
+            std::vector<std::thread> threads(noThreads);
+
+            for (int t = 0; t < noThreads; ++t) {
+                threads[t] = std::thread([&]() {
+                    long double a = 1.0, b = 1.0 / sqrt(2.0), t = 1.0 / 4.0, p = 1.0;
+                    long double piPrev = 0.0, pi = 0.0;
+
+                    int iterations_per_thread = ceil(
+                            log2(gaussLegendreCPU::noDecimals / 14.6) /
+                            noThreads); // Compute number of iterations per thread
+
+                    int start_iteration = t * iterations_per_thread;
+                    int end_iteration = (t + 1) * iterations_per_thread;
+                    if (t == noThreads - 1) {
+                        end_iteration = ceil(log2(gaussLegendreCPU::noDecimals / 14.6));
+                    }
+
+                    for (int i = start_iteration; i < end_iteration; ++i) {
+                        if (gaussLegendreCPU::cancel) {
+                            break; //if the cancel command is given we stop iterating
+                        }
+
+                        long double aNext = (a + b) / 2.0;
+                        long double bNext = sqrt(a * b);
+                        long double tNext = t - p * pow(a - aNext, 2.0);
+                        long double pNext = 2.0 * p;
+
+                        a = aNext;
+                        b = bNext;
+                        t = tNext;
+                        p = pNext;
+
+                        piPrev = pi;
+                        pi = pow(a + b, 2.0) / (4.0 * t);
+                        if (pi == piPrev) {
+                            break; // Stop iterating if no change in pi
+                        }
+                    }
+                });
+            }
+        }
+        void gaussLegendreCPU::runAbsolute(bool multithreading){
+            if (multithreading) {
+                gaussLegendreCPU::runMultiThreaded(gaussLegendreCPU::noThreads);
+            } else {
+                gaussLegendreCPU::runSingleThread();
+            }
+        }
+        //namespace Cpu
     }
-    else{
-        gaussLegendreCPU::runsinglethread();
-    }
+    //namespace Benchmark
 }
