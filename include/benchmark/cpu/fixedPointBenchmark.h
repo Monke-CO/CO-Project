d3cc1748a06b31a519a4e32990b7309d814ce85c
marc <fmarcandrei@gmail.com>
2023-05-11 01:54:26 +0300

    added namespaces and modified runAbsolute

---
+++ include/benchmark/cpu/fixedPointBenchmark.h
@@ -0,0 +1,35 @@
+//
+// Created by marc on 5/10/23.
+//
+
+#ifndef CO_PROJECT_FIXEDPOINTBENCHMARK_H
+#define CO_PROJECT_FIXEDPOINTBENCHMARK_H
+
+namespace BenchMark {
+    namespace Cpu {
+        class fixedPointBenchmark {
+        private:
+            int size;
+            bool cancel;
+        public:
+            void Cancel(bool cance);
+
+            void Run();
+
+            void SetSize(int siz);
+
+            void IntegerArithmeticTest();
+
+            void BranchingTest();
+
+            void ArrayAccessTest();
+
+            void GetResult();
+        };
+        //namespace cpu
+    }
+    //namespace benchmark
+}
+
+
+#endif //CO_PROJECT_FIXEDPOINTBENCHMARK_H
