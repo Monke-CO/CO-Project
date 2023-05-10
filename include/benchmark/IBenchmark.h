b620819b128c49369a026e823b5620b498cd6bf7
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2023-05-10 20:07:22 +0300

    Add benchmark namespace

--- include/benchmark/IBenchmark.h
+++ include/benchmark/IBenchmark.h
@@ -2,13 +2,17 @@
 // Created by alex on 21.04.2023.
 //
 #pragma once
+namespace Benchmark {
+    class IBenchmark {
+    public:
+        virtual ~IBenchmark() = default;
 
-class IBenchmark {
-public:
-    virtual ~IBenchmark() = default;
+        virtual void run() = 0;
 
-    virtual void run() = 0;
-    virtual void initialize() = 0;
-    virtual void cleanup() = 0;
-    virtual void cancel() = 0;
-};
+        virtual void initialize() = 0;
+
+        virtual void cleanup() = 0;
+
+        virtual void cancel() = 0;
+    };
+}
